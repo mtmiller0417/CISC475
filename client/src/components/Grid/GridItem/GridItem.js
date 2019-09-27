e14310e004f71712ac8a574a8f05568cdf821c19
Dan Paddock <danpaddk@gmail.com>
2019-09-27 14:52:22 -0400

    added graphing file, loadings graphs in each grid tile

--- client/src/components/Grid/GridItem/GridItem.js
+++ client/src/components/Grid/GridItem/GridItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './GridItem.module.scss';
+import Graph from '../../Graph'
 
 export default class GridItem extends React.Component {
   render() {
     return(
       <div class={styles.gridItem}>
-        Grid Item
+        <Graph />
       </div>
     );
   }
