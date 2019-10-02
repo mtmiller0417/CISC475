c6ff907ac0d2c8687a848af1905a6d99061cb439
Matt <matt.miller041798@gmail.com>
2019-10-02 13:24:40 -0400

    Added some import d3 compatability in the graph.js file

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import {Line} from 'react-chartjs-2';
+import * as d3 from 'd3';
 
 class Graph extends Component{
     constructor(props){
         super(props);
+        d3.csv("CISC475/ECG Dataset/13013356000.csv",function(data){
+            // Write code dealing with data
+            console.log(data.data);
+        });
         this.state = {
             graphData:{
-            labels: ['Boston', 'New York', 'Paris'],
-            datasets:[
+                labels: ['Boston', 'New York', 'Paris'],
+                datasets:[
                 {
-                      label:'Population',
-                      data:[3234, 5637, 50],
-                      backgroundColor:[
-                        'rgba(255,99,132,0.6)',
-                      ]
+                    label:'Population',
+                    data:[3234, 5637, 50],
+                    backgroundColor:['rgba(255,99,132,0.6)',]
                       
                 }
             ]
@@ -22,12 +25,12 @@ class Graph extends Component{
 }
     
     render(){
-            return(
+        return(
             <div className="graph">
-                   <Line
-                   data={this.state.graphData}
-                   options={{ maintainAspectRatio: false }}
-                   />
+                <Line
+                    data={this.state.graphData}
+                    options={{ maintainAspectRatio: false }}
+               />
             </div>
          )
     }
