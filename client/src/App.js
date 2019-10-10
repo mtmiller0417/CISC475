9e125793dde1cd96af7c31a0cd954161f0455d70
charleslambert98 <clambert@udel.edu>
2019-10-09 23:08:10 -0400

    Overhauled Metadata and Neatened Formatting of UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 // We have created a component called MainContainer.js in our directory.
 // In order to use it in App.js, we have to import it. Note that you don't
 // need to add .js to the file name at the end (so the last part doesn't have
 // to be /MainContainer.js)
-import MainContainer from './components/MainContainer/MainContainer';
-import Header from './components/Header/Header';
+import MainContainer from "./components/MainContainer/MainContainer";
+import Header from "./components/Header/Header";
+import Metadata from "./components/Metadata/Metadata";
+import Grid from "./components/Grid/Grid";
 
 function App() {
-  return (
-    <div className="App">
-      {
-        // This comment is in reference to using MainContainer below. As you can see,
-        // we use it just like an HTML tag, except we can pass in "props". In this case
-        // we are passing a random message in it. In MainContainer.js, you can see how we
-        // use this field
-      }
-      <Header />
-      <MainContainer message="Hello World" />
-    </div>
-  );
+	return (
+		<div className="App">
+			{
+				// This comment is in reference to using MainContainer below. As you can see,
+				// we use it just like an HTML tag, except we can pass in "props". In this case
+				// we are passing a random message in it. In MainContainer.js, you can see how we
+				// use this field
+			}
+			<Grid>
+				<Metadata />
+				<Header />
+			</Grid>
+			<MainContainer message="Hello World" />
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/MainContainer/MainContainer.js
+++ client/src/components/MainContainer/MainContainer.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import styles from './MainContainer.module.scss';
-import Grid from '../Grid/Grid';
-import GridItem from '../Grid/GridItem/GridItem';
-import Metadata from '../Metadata/Metadata';
+import React from "react";
+import styles from "./MainContainer.module.scss";
+import Grid from "../Grid/Grid";
+import GridItem from "../Grid/GridItem/GridItem";
 
 export default class MainContainer extends React.Component {
-  render() {
-    return(
-      <div className={styles.container}>
-           <Grid>
-              <Metadata />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-              <GridItem />
-           </Grid>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className={styles.container}>
+				<Grid>
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+					<GridItem />
+				</Grid>
+			</div>
+		);
+	}
 }
