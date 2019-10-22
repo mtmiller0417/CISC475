d922f1014670b29a09165cfae4d04ff9abb3c88d
Matt <matt.miller041798@gmail.com>
2019-10-21 23:33:16 -0400

    Added time stamp and working towards graphing annotations

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -9,21 +9,6 @@ class Graph extends Component{
     constructor(props){
         super(props);
 
-        /*this.state = ({
-            graphData:{
-                labels: this.props.inputArr.labels,
-                datasets:[{
-                    radius: 0, // Makes the dots go away
-                    label: this.props.inputArr.title,
-                    fill: false,
-                    borderColor: ['black'],
-                    data: this.props.inputArr.data,
-                    backgroundColor:['rgba(255,99,132,0.6)',],
-                    borderWidth: 1
-                }]
-            }
-        })*/
-
         this.state = ({
             data:{
                 datasets:{
@@ -194,14 +179,13 @@ class Graph extends Component{
     render(){
 
         console.log('RENDER in Graph.js')
-        console.log('State after update')
         console.log(this.state)
 
         let skip = 500
         // Need 200 ms or 0.2 s
         if(this.state.data.frequency > 0){
             skip = this.state.data.frequency * 0.2; // 0.2 seconds = 200 ms
-            console.log("skip: " + skip)
+            //console.log("skip: " + skip)
         }
 
 
@@ -209,7 +193,7 @@ class Graph extends Component{
             labels: ['Scatter'],
             datasets: [
               {
-                label: 'My First dataset',
+                //label: 'My First dataset',
                 fill: false,
                 backgroundColor: 'rgba(75,192,192,0.4)',
                 pointBorderColor: 'rgba(75,192,192,1)',
@@ -229,6 +213,32 @@ class Graph extends Component{
             ], 
           };
 
+          const p_graph = {
+            labels: ['Scatter'],
+            datasets: [
+              {
+                pointStyle: 'star',
+                fill: false,
+                backgroundColor: 'red',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 1,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 1,
+                pointRadius: 0, // pointRadius: 1
+                pointHitRadius: 10,
+                borderWidth: 1,
+                borderColor:'black',
+                showLine: false,
+                data: this.state.data.annotation.p
+              }
+            ], 
+          };
+
+          
+
         return(
         <React.Fragment>
             {
@@ -239,7 +249,7 @@ class Graph extends Component{
                         options={{
                             title: {
                                 display: true,
-                                text: this.props.inputArr.title,
+                                text: this.state.data.datasets.label,
                                 fontSize: 18,
                                 fontFamily: "sans-serif"
                             },
@@ -253,9 +263,9 @@ class Graph extends Component{
                                         stepSize: 0.2//skip Measured in seconds
                                     },
                                     // Vertical grid-lines
-                                    gridLines: {
+                                    /*gridLines: {
                                         display: true,
-                                    },
+                                    },*/
                                     scaleLabel: {
                                         display: true,
                                         labelString: 'Time in Seconds'
@@ -263,7 +273,7 @@ class Graph extends Component{
                                 }],
                                 yAxes: [{
                                     ticks: {
-                                        //stepSize: skip
+                                        stepSize: 500
                                     },
                                     gridLines: {
                                         display: true
